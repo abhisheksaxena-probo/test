23cdb904182af01bc8826db0aece9305e4db70ad
Abhishek <abhishek.saxena@probo.in>
2024-01-22 16:01:23 +0530

    github-actions added

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,11 @@ COPY package*.json ./
 
 RUN npm install
 
+RUN npm install sequelize-cli --save
+
 COPY . .
+
+
 
 EXPOSE 7050
 
--- db/config/config.js
+++ db/config/config.js
@@ -1,35 +1,14 @@
 const env = require("../../utils/environment");
-
-const read = [];
-const readReplicaHosts = [0];
-readReplicaHosts.map((host) =>
-  read.push({
-    username: env.MY_SQL_USER,
-    password: env.MY_SQL_PASSWORD,
-    database: env.MY_SQL_DB,
-    host: env.MY_SQL_HOST,
-  })
-);
+console.log(env.DB_DIALECT);
+console.log(env.MY_SQL_DB);
+console.log(env.MY_SQL_USER);
+console.log(env.MY_SQL_PASSWORD);
 
 const config = {
   dialect: env.DB_DIALECT,
-  host: env.MY_SQL_HOST,
-  replication: {
-    read: read,
-    write: {
-      username: env.MY_SQL_USER,
-      password: env.MY_SQL_PASSWORD,
-      database: env.MY_SQL_DB,
-      host: env.MY_SQL_HOST,
-    },
-  },
-  pool: {
-    max: parseInt(env.DB_POOL_MAX, 10),
-    min: parseInt(env.DB_POOL_MIN, 10),
-    acquire: parseInt(env.DB_POOL_LOCK_ACQUIRE, 10),
-    idle: parseInt(env.DB_POOL_IDLE, 10),
-  },
-  logging: env.DB_LOGGING === "true",
+  database: env.MY_SQL_DB,
+  username: env.MY_SQL_USER,
+  password: env.MY_SQL_PASSWORD,
 };
 
 module.exports = config;
